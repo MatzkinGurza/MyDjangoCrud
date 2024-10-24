4c2c8d39ffb893a40a46e7d674e0cac8e52eb49f
MatzkinGurza <gurza.mateus@usp.br>
2024-10-24 18:31:28 -0300

    added blog posts to homepage and detailed view

--- CrudBlog/urls.py
+++ CrudBlog/urls.py
@@ -1,6 +1,11 @@
-from . import views
+from .views import HomeView, ArticleDetailView
+#from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name="home")
+    #path('', views.home, name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    #<int:pk> completes the url with the primary key of the blog entry (assigned on creation)
+
 ]
