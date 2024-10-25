e85453fde48de1bd5d5eb2ad0bae674309bc086e
MatzkinGurza <gurza.mateus@usp.br>
2024-10-25 00:50:13 -0300

    created add blog post functionality

--- CrudBlog/urls.py
+++ CrudBlog/urls.py
@@ -1,4 +1,4 @@
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, AddPostView
 #from . import views
 from django.urls import path
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
     #<int:pk> completes the url with the primary key of the blog entry (assigned on creation)
+    path('add_post/', AddPostView.as_view(), name='add_post')
 
 ]
