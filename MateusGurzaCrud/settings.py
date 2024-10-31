a322d93559cc9da72870481cb6eefbeb593b0264
MatzkinGurza <gurza.mateus@usp.br>
2024-10-31 03:24:16 -0300

    added auth login logout and register - all working

--- MateusGurzaCrud/settings.py
+++ MateusGurzaCrud/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "CrudBlog"
+    "CrudBlog",
+    "members",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
--- MateusGurzaCrud/urls.py
+++ MateusGurzaCrud/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('CrudBlog.urls'))
+    path('', include('CrudBlog.urls')),
+    path('members/', include('django.contrib.auth.urls')), #this includes the login url directly to the blog
+    path('members/', include('members.urls')),
+
 ]
