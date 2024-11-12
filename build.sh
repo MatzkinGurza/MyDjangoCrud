cfabe018f8b3be895af2c7d155e7dc653a42adec
MatzkinGurza <gurza.mateus@usp.br>
2024-11-12 14:16:49 -0300

    render 8th try

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py mmakemigrations  # Adicione este comando para aplicar as migrações
-python manage.py migrate  # Adicione este comando para aplicar as migrações
+
 python manage.py collectstatic --no-input
+
+python manage.py makemigrations  # Adicione este comando para aplicar as migrações
+python manage.py migrate  # Adicione este comando para aplicar as migrações
